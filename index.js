32f68ec92182f861b50682ddd36a600bffb7dd87
Jhonatan Restrepo <jhonatan10rpo@gmail.com>
2022-06-13 14:57:50 -0500

    this is a practice of the os module

---
+++ index.js
@@ -0,0 +1,7 @@
+const os = require('os');
+
+console.log(os.platform());
+console.log(os.release());
+console.log('Free mem',os.freemem(), 'bytes');
+console.log('Total mem',os.totalmem(), 'bytes');
+
